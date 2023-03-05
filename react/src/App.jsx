565c5515a72b2e7022c4b8cee0e03f6dec11e756
NGR <72463407+NGR-NP@users.noreply.github.com>
2023-03-05 21:03:58 +0545

    commit

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("")
+  const [data, setData] = useState([])
+
+  const handleChange = (e) => {
+    setData(e.target.value)
+  }
+  const handleSubmit = (e) => {
+
+    setTodo('')
+  }
+  useEffect(() => {
+    if (data === 0) {
+      empty.style.display = 'block'
+    } else {
+
+    }
+  }, [data])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <section class="sec">
+      <h1 class="title">Todo App</h1>
+      <form class="todoForm">
+        <input value={todo} onChange={handleChange} type="text" class="formInput" id="newTask" />
+        <button onClick={handleSubmit} type="button" class="addBtn" id="addTask">Add Task</button>
+      </form>
+      <fieldset class="lists">
+        <legend>
+          My Todo List
+        </legend>
+        <div id="empty">No task on the list</div>
+        <ul id="todoList"></ul>
+      </fieldset>
+    </section>
   )
 }
 
